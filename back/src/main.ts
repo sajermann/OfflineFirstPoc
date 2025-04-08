32ff50baa5d9e6f1ae0f1aa31304253b2dad630a
Sajermann <sajermannbruno@gmail.com>
2025-04-08 12:46:29 -0300

    fix

--- back/src/main.ts
+++ back/src/main.ts
@@ -18,6 +18,7 @@ async function bootstrap() {
       'http://127.0.0.1:5000',
       'http://localhost:5001',
       'http://127.0.0.1:5001',
+      'https://sajermann.github.io',
       // 'http://192.168.15.5:5173',
       // 'http://localhost:4200',
     ],
